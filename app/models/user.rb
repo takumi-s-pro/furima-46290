3089c0fcd8363bb12a503d7dcf083261505b80e8
takumi-s-prog <takumi.s-lovelovelive@docomo.ne.jp>
2025-08-29 15:27:27 +0900

    未入力箇所のエラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,18 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   # 名前（全角ひらがな・カタカナ・漢字）
-  validates :last_name,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "は全角で入力してください" }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "は全角で入力してください" }
+  validates :last_name,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "is invalid. Input full-width characters" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "is invalid. Input full-width characters" }
 
   # フリガナ（全角カタカナ）
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
 
   # 誕生日必須
   validates :birthday, presence: true
 
   # パスワード
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/i, message: "は6文字以上で英字と数字の両方を含めて設定してください" }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/i, message: "is invalid. Include both letters and numbers" }
 
 
   has_many :items
