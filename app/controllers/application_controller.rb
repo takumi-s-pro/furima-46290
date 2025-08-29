569892a842298dd91ddb96ab7bf12a0327a1feaa
takumi-s-prog <takumi.s-lovelovelive@docomo.ne.jp>
2025-08-29 17:03:08 +0900

    ページ遷移に関するテストコードの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
   end
 
   def basic_auth
+    return if Rails.env.test?
+    
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe "トップページ ヘッダー", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  let!(:user) { FactoryBot.create(:user) }
+
+  context "ログアウト状態の場合" do
+    it "新規登録とログインボタンが表示される" do
+      visit root_path
+      expect(page).to have_link "新規登録", href: new_user_registration_path
+      expect(page).to have_link "ログイン", href: new_user_session_path
+      expect(page).not_to have_content user.nickname
+      expect(page).not_to have_link "ログアウト"
+    end
+
+    it "新規登録ボタンをクリックすると新規登録ページに遷移する" do
+      visit root_path
+      click_link "新規登録"
+      expect(current_path).to eq new_user_registration_path
+    end
+
+    it "ログインボタンをクリックするとログインページに遷移する" do
+      visit root_path
+      click_link "ログイン"
+      expect(current_path).to eq new_user_session_path
+    end
+  end
+
+  context "ログイン状態の場合" do
+    before do
+      visit new_user_session_path
+      fill_in "email", with: user.email
+      fill_in "password", with: user.password
+      click_button "ログイン"
+  end
+
+    it "ユーザーのニックネームとログアウトボタンが表示される" do
+      visit root_path
+      expect(page).to have_content user.nickname
+      expect(page).to have_link "ログアウト"
+    end
+
+    it "ログアウトボタンをクリックするとログアウトできる" do
+      visit root_path
+      click_link "ログアウト"
+      expect(page).to have_link "新規登録"
+      expect(page).to have_link "ログイン"
+    end
+  end
+end
