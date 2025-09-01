0a0b3e6f6e941bd926ed0939fc1afd5ee3f332ee
takumi-s-prog <takumi.s-lovelovelive@docomo.ne.jp>
2025-09-01 18:34:03 +0900

    金額入力時に販売手数料や販売利益の計算結果が表示

---
+++ app/javascript/price_calculation.js
@@ -0,0 +1,17 @@
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+
+  if (!priceInput) return;
+
+  priceInput.addEventListener("input", () => {
+    const price = Number(priceInput.value);
+
+    const fee = Math.floor(price * 0.1);
+    const gain = price - fee;
+
+    addTaxDom.innerHTML = fee.toLocaleString();
+    profitDom.innerHTML = gain.toLocaleString();
+  });
+});
