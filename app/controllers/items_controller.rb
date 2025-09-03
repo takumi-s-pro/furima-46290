45150b7795eb3cfc5a7a70cbc6b01e44efbd0d67
takumi-s-prog <takumi.s-lovelovelive@docomo.ne.jp>
2025-09-03 15:44:35 +0900

    出品者以外が編集ページへ遷移できない機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -44,5 +45,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def ensure_correct_user
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
