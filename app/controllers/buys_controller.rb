ff2cf61b9866b53dca2cb73d17b68881dea32944
takumi-s-prog <takumi.s-lovelovelive@docomo.ne.jp>
2025-09-04 16:03:34 +0900

    コントローラー調整

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,6 +1,6 @@
 class BuysController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_root, only: [:index, :create]
 
   def index
@@ -10,12 +10,7 @@ class BuysController < ApplicationController
   def create
     @buy = Buy.new(buy_params)
     if @buy.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-      amount: @item.price,
-      card: buy_params[:token],
-      currency: 'jpy'
-      )
+      pay_item
       @buy.save
       redirect_to root_path
     else
@@ -39,5 +34,14 @@ class BuysController < ApplicationController
     if current_user.id == @item.user_id || @item.buy.present?
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buy_params[:token],
+      currency: 'jpy'
+    )
   end
 end
