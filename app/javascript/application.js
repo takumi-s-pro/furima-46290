3e44a07b0e94141c21d73a81bb655b2609e492df
takumi-s-prog <takumi.s-lovelovelive@docomo.ne.jp>
2025-09-08 18:36:13 +0900

    手数料表示修正#2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./price_calculation"
-import "./card"
+import "price_calculation"
+import "card"
 
